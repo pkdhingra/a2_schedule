46e655fff3c7fe2c0779ae4e03bba890c9b3b049
hetdataknobs <het.p@dataknobs.com>
2025-04-10 05:46:31 +0000

    Add files via upload

---
+++ runsite/runsite2_updated.bat
@@ -0,0 +1,258 @@
+@echo off
+echo List of websites that will be opened:
+echo https://www.kreatebots.com/
+echo https://www.kreatewebsites.com/
+echo https://slides.kreatebots.com/
+echo https://www.1000placestosee.info/
+echo https://www.1000placestovisit.org/
+echo https://www.apartments-sale.com/
+echo https://www.apartments-sale.net/
+echo https://www.beautyparlors.org/
+echo https://slides.kreatewebsites.com/
+echo https://www.lohri.org/
+echo https://products.beautyparlors.org/
+echo https://www.bestech.org/
+echo https://www.chandnichowk.info/
+echo https://www.chinacitytrip.com/
+echo https://www.connaught-place.com/
+echo https://www.connaughtplace.org/
+echo https://www.dataproduct.net/
+echo https://www.daynightcricket.com/
+echo https://www.dhingra.website/
+echo https://www.europecitytrip.com/
+echo https://www.fishermanwharf.org/
+echo https://www.happydeepawali.org/
+echo https://www.indiacities.net/
+echo https://www.indiafort.org/
+echo https://www.indianmovies.us/
+echo https://www.lowback.org/
+echo https://www.makarsankranti.org/
+echo https://www.niagarafall.info/
+echo https://www.nri-tax.com/
+echo https://www.olddelhi.org/
+echo https://places-to-see.net/
+echo https://www.qutubminar.org/
+echo https://www.roadhana.com/
+echo https://www.shirditrip.com/
+echo https://www.srisaibaba.org/
+echo https://www.tamilmovies.us/
+echo https://www.tax-income.com/
+echo https://www.tax-income.net/
+echo https://www.tax-income.org/
+echo https://www.taxs.info/
+echo https://www.uscitytrip.com/
+echo https://www.valentineday.mobi/
+echo https://www.yellp.net/
+echo https://www.kreate.cloud/
+echo.
+echo Starting website rotation in 5 seconds...
+timeout /t 5
+
+:loop
+
+	call start msedge "https://www.kreatebots.com/"
+				timeout /t 5
+	call start msedge "https://www.kreatewebsites.com/"
+				timeout /t 5
+    call start msedge "https://slides.kreatebots.com/"
+	call start msedge "https://www.kreatebots.com/"
+	call start msedge "https://www.1000placestosee.info/"
+			timeout /t 15
+	call start msedge "https://www.1000placestovisit.org/"
+			timeout /t 15
+	call start msedge "https://www.1000placestovisit.org/"
+			timeout /t 15
+	call start msedge "https://www.1000placestosee.info"
+			timeout /t 25
+
+	call start msedge "https://www.apartments-sale.com/"
+			timeout /t 13
+	call start msedge "https://www.apartments-sale.net/"
+			timeout /t 35
+	call start msedge "https://www.beautyparlors.org/"
+			timeout /t 5
+			
+			timeout /t 50
+	call start msedge "https://slides.kreatewebsites.com/"
+			timeout /t 15
+	call start msedge "https://www.lohri.org/"
+			timeout /t 15
+	call start msedge "https://www.1000placestovisit.org/"
+			timeout /t 15
+	call start msedge "https://www.1000placestosee.info"
+			timeout /t 25
+
+	call start msedge "https://www.apartments-sale.com/"
+			timeout /t 13
+	call start msedge "https://www.apartments-sale.net/"
+			timeout /t 35
+	call start msedge "https://www.beautyparlors.org/"
+			timeout /t 5
+			
+	call start msedge "https://products.beautyparlors.org/"
+			timeout /t 11
+	call start msedge "https://www.bestech.org/"
+			timeout /t 8
+	call start msedge "https://www.chandnichowk.info/"
+			timeout /t 9
+	call start msedge "https://www.chinacitytrip.com/"
+			timeout /t 12
+	call start msedge "https://www.connaught-place.com/"
+			timeout /t 15
+	call start msedge "https://www.connaughtplace.org/"
+			timeout /t 5
+	call start msedge "https://www.dataproduct.net/"
+			timeout /t 10
+
+	call start msedge "https://www.daynightcricket.com/"
+			timeout /t 5
+	call start msedge "https://www.dhingra.website/"
+			timeout /t 5
+	call start msedge "https://www.europecitytrip.com/"
+			timeout /t 5
+	call start msedge "https://www.fishermanwharf.org/"
+		timeout /t 5
+
+	
+	call start msedge "https://www.happydeepawali.org/"
+		timeout /t 5
+
+	
+		
+	call start msedge "https://www.indiacities.net/"
+		timeout /t 5
+	call start msedge "https://www.indiafort.org/"
+		timeout /t 5
+	call start msedge "https://www.indianmovies.us/"
+		timeout /t 5
+	
+			
+	call start msedge "https://www.kreatebots.com/"
+		timeout /t 5
+	call start msedge "https://www.lohri.org/"
+		timeout /t 5
+	call start msedge "https://www.lowback.org/"
+		timeout /t 5
+
+	call start msedge "https://www.makarsankranti.org/"
+		timeout /t 5
+	call start msedge "https://www.niagarafall.info/"
+		timeout /t 5
+	
+	call start msedge "https://www.nri-tax.com/"
+		timeout /t 5
+	call start msedge "https://www.olddelhi.org/"
+		timeout /t 5
+	call start msedge "https://places-to-see.net/"
+		timeout /t 5
+	call start msedge "https://www.qutubminar.org/"
+		timeout /t 5
+	call start msedge "https://www.roadhana.com/"
+		timeout /t 5
+
+	call start msedge "https://www.shirditrip.com/"
+		timeout /t 5
+	call start msedge "https://www.srisaibaba.org/"
+		timeout /t 5
+	call start msedge "https://www.tamilmovies.us/"
+		timeout /t 5
+		
+	call start msedge "https://www.tax-income.com/"
+		timeout /t 5
+	call start msedge "https://www.tax-income.net/"
+		timeout /t 5
+	call start msedge "https://www.tax-income.org/"
+		timeout /t 5
+		
+	call start msedge "https://www.taxs.info/"
+		timeout /t 5
+
+	call start msedge "https://www.uscitytrip.com/"
+		timeout /t 5
+		
+	call start msedge "https://www.valentineday.mobi/"
+		timeout /t 5
+	call start msedge "https://www.yellp.net/
+		timeout /t 5
+
+	call start msedge "https://www.1000placestosee.info/"
+	timeout /t 7
+	call start msedge "https://www.1000placestovisit.org/"
+	
+	timeout /t 7
+	call start msedge "https://www.apartments-sale.com/"
+	timeout /t 7
+	call start msedge "https://www.apartments-sale.net/"
+        timeout /t 7
+
+    taskkill /F /IM msedge.exe
+
+	call start msedge "https://www.beautyparlors.org/"
+	timeout /t 7
+	call start msedge "https://www.bestech.org/"
+        timeout /t 7
+	call start msedge "https://www.chandnichowk.info/"
+	timeout /t 7
+	call start msedge "https://www.chinacitytrip.com/"
+        timeout /t 7
+	call start msedge "https://www.chandnichowk.info/"
+        timeout /t 7
+	call start msedge "https://www.dhingra.website/"
+        timeout /t 7
+	call start msedge "https://www.europecitytrip.com/"
+        timeout /t 7
+	call start msedge "https://www.fishermanwharf.org/"
+	timeout /t 5
+	timeout /t 7
+
+	call start msedge "https://www.indiacities.net/"
+        timeout /t 7
+	call start msedge "https://www.indiafort.org/"
+        timeout /t 7
+	call start msedge "https://www.kreatebots.com/"
+        timeout /t 7
+	call start msedge "https://www.makarsankranti.org/"
+        timeout /t 7
+	call start msedge "https://www.niagrafall.info/"
+        timeout /t 7
+
+	call start msedge "https://www.uscitytrip.com/"
+        timeout /t 7
+	call start msedge "https://www.valentineday.mobi/"
+        timeout /t 7
+	call start msedge "https://www.yellp.net/"
+        timeout /t 7
+	call start msedge "https://www.uscitytrip.com/"
+	
+	taskkill /F /IM msedge.exe
+	
+	call start msedge "https://www.places-to-see.net/"
+        timeout /t 7
+	call start msedge "https://www.srisaibaba.org/"
+        timeout /t 7
+	call start msedge "https://www.kreatebots.com/"
+        timeout /t 7
+	call start msedge "https://www.tax-income.net/"
+        timeout /t 7
+	call start msedge "https://www.tax-income.com/"
+	timeout /t 5
+
+
+	call start msedge "https://www.makarsankranti.org/"
+	timeout /t 7
+
+	call start msedge "https://www.lowback.org/"
+	timeout /t 5
+	call start msedge "https://www.bestech.org/"
+	timeout /t 5
+	call start msedge "https://www.kreate.cloud"
+	timeout /t 5
+	call start msedge "https://www.dataproduct.net"
+	timeout /t 5
+
+
+	taskkill /F /IM msedge.exe
+
+	timeout /t 50
+	
+	goto loop
